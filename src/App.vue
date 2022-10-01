0aa40b3a0fede3fbca5b6b69d70a8d0f0a0e7dea
Poka Hsiao <53artworkerstudio@gmail.com>
2022-10-01 19:19:50 +0800

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <div id="nav" class="nav">
+      <router-link to="/">About</router-link> 
+      <router-link to="/Experience">Experience</router-link> 
+      <router-link to="/Project">Project</router-link>
+      <router-link to="/Expertise">Expertise</router-link> 
+      <router-link to="/UIUX">UI/UX</router-link> 
+    </div>
+
+<div class="box">
+
+          <router-view />
+
+</div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () { 
+    return { 
+      index:0
+       } },
+}
+</script>
+
+<style lang="postcss">
+body{
+  @apply bg-myYellow;
+}
+
+#app {
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  @apply flex flex-col items-center;
+}
+
+.nav {@apply text-center  flex justify-center overflow-hidden h-24 py-2
+              w-full border border-pink-500;}
+.nav a{@apply pt-8 pb-36 mr-1 w-32 mt-4 
+            text-myBrown font-bold border border-white
+            bg-myBrown-light
+            rounded-full text-center;}
+.nav a:hover{@apply transition ease-in duration-100 text-white bg-myBrown mt-2;}
+.nav .router-link-exact-active{@apply  mt-0
+text-myBrown-dark  border-myBrown
+bg-myBlue pointer-events-none;}
+
+
+</style>
---
+++ src/views/UIUX.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="UIUX ContainerSpacing">
+        <TitleBar
+        :PageName ="PageName" />
+
+        <div class="grid grid-cols-3 gap-4 ">
+            <div class="bg-lime-400">05</div>
+            <div class="col-span-2 bg-red-400">04</div>
+        </div>
+
+</div>
+</template>
+
+<script>
+import  TitleBar from "@/components/TitleBar.vue";
+
+export default {
+    name: "UIUX",
+    components: { TitleBar },
+    data() {
+        return {
+            PageName:'UI/UX',
+        }
+    },
+}
+</script>
+
+<style lang="postcss">
+</style>
